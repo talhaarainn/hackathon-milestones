dc8ac0f068f6b8d83153ca950ddf214d918658f5
Talha Arain <talha81664@gmail.com>
2024-11-01 05:08:55 -0700

    Add files via upload

---
+++ milestone3/script.js
@@ -0,0 +1,21 @@
+var form = document.getElementById('resume-form');
+var resumeDispaly = document.getElementById('resume-display');
+// Form Submission
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var phone = document.getElementById('phone').value;
+    var education = document.getElementById('education').value;
+    var experience = document.getElementById('experience').value;
+    var skills = document.getElementById('skills').value;
+    //Generate Dynamic Resume
+    var resume = "\n    <h2><b>Resume</b></h2>\n    <h3>Personal Information</h3>\n    <p><b>Name:</b>".concat(name, "</p>\n    <p><b>Email:</b>").concat(email, "</p>\n    <p><b>Phone:</b>").concat(phone, "</p>\n\n    <h3>Education</h3>\n    <p>").concat(education, "</p>\n\n    <h3>Experience</h3>\n    <p>").concat(experience, "</p>\n\n    <h3>Skills</h3>\n    <p>").concat(skills, "</p>\n    ");
+    //Display resume
+    if (resumeDispaly) {
+        resumeDispaly.innerHTML = resume;
+    }
+    else {
+        console.error('Resume element is missing');
+    }
+});
---
+++ milestone3/script.ts
@@ -0,0 +1,42 @@
+const form = document.getElementById('resume-form') as HTMLFormElement;
+const resumeDispaly = document.getElementById('resume-display') as HTMLDivElement;
+
+// Form Submission
+
+form.addEventListener('submit', (event: Event) => {
+    event.preventDefault();
+
+    const name = (document.getElementById('name') as HTMLInputElement).value
+    const email = (document.getElementById('email') as HTMLInputElement).value
+    const phone = (document.getElementById('phone') as HTMLInputElement).value
+    const education = (document.getElementById('education') as HTMLInputElement).value
+    const experience = (document.getElementById('experience') as HTMLInputElement).value
+    const skills = (document.getElementById('skills') as HTMLInputElement).value
+
+    //Generate Dynamic Resume
+
+    const resume = `
+    <h2><b>Resume</b></h2>
+    <h3>Personal Information</h3>
+    <p><b>Name:</b>${name}</p>
+    <p><b>Email:</b>${email}</p>
+    <p><b>Phone:</b>${phone}</p>
+
+    <h3>Education</h3>
+    <p>${education}</p>
+
+    <h3>Experience</h3>
+    <p>${experience}</p>
+
+    <h3>Skills</h3>
+    <p>${skills}</p>
+    `;
+
+    //Display resume
+
+    if(resumeDispaly){
+        resumeDispaly.innerHTML = resume;
+    } else {
+        console.error('Resume element is missingi')
+    }
+})
